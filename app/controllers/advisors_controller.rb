cde4bb65a9ffe997f5543f283268cb220cb30ce4
damanza2144 <damanza@unm.edu>
2012-12-04 21:19:03 -0700

    formatting of some of the advisor rails code

--- app/controllers/advisors_controller.rb
+++ app/controllers/advisors_controller.rb
@@ -1,83 +1,104 @@
 class AdvisorsController < ApplicationController
-  # GET /advisors
-  # GET /advisors.json
-  def index
-    @advisors = Advisor.all
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @advisors }
-    end
-  end
+	# GET /advisors
+	# GET /advisors.json
+	def index
 
-  # GET /advisors/1
-  # GET /advisors/1.json
-  def show
-    @advisor = Advisor.find(params[:id])
+		@advisors = Advisor.all
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @advisor }
-    end
-  end
+		respond_to do |format|
+			format.html # index.html.erb
+			format.json { render json: @advisors }
+		end
+	end
 
-  # GET /advisors/new
-  # GET /advisors/new.json
-  def new
-    @advisor = Advisor.new
+	# GET /advisors/1
+	# GET /advisors/1.json
+	def show
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @advisor }
-    end
-  end
+		@advisor = Advisor.find(params[:id])
 
-  # GET /advisors/1/edit
-  def edit
-    @advisor = Advisor.find(params[:id])
-  end
+		respond_to do |format|
+			format.html # show.html.erb
+			format.json { render json: @advisor }
+		end
+	end
 
-  # POST /advisors
-  # POST /advisors.json
-  def create
-    @advisor = Advisor.new(params[:advisor])
+	# GET /advisors/new
+	# GET /advisors/new.json
+	def new
 
-    respond_to do |format|
-      if @advisor.save
-        format.html { redirect_to @advisor, notice: 'Advisor was successfully created.' }
-        format.json { render json: @advisor, status: :created, location: @advisor }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @advisor.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+		@advisor = Advisor.new
 
-  # PUT /advisors/1
-  # PUT /advisors/1.json
-  def update
-    @advisor = Advisor.find(params[:id])
+		respond_to do |format|
+			format.html # new.html.erb
+			format.json { render json: @advisor }
+		end
+	end
 
-    respond_to do |format|
-      if @advisor.update_attributes(params[:advisor])
-        format.html { redirect_to @advisor, notice: 'Advisor was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @advisor.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+	# GET /advisors/1/edit
+	def edit
 
-  # DELETE /advisors/1
-  # DELETE /advisors/1.json
-  def destroy
-    @advisor = Advisor.find(params[:id])
-    @advisor.destroy
+		@advisor = Advisor.find(params[:id])
 
-    respond_to do |format|
-      format.html { redirect_to advisors_url }
-      format.json { head :no_content }
-    end
-  end
+	end
+
+	# POST /advisors
+	# POST /advisors.json
+	def create
+
+		@advisor = Advisor.new(params[:advisor])
+
+		respond_to do |format|
+
+			if @advisor.save
+
+				format.html { redirect_to @advisor, notice: 'Advisor was successfully created.' }
+				format.json { render json: @advisor, status: :created, location: @advisor }
+
+			else
+
+				format.html { render action: "new" }
+				format.json { render json: @advisor.errors, status: :unprocessable_entity }
+
+			end
+		end
+	end
+
+	# PUT /advisors/1
+	# PUT /advisors/1.json
+	def update
+
+		@advisor = Advisor.find(params[:id])
+
+		respond_to do |format|
+
+			if @advisor.update_attributes(params[:advisor])
+        
+				format.html { redirect_to @advisor, notice: 'Advisor was successfully updated.' }
+				format.json { head :no_content }
+
+			else
+
+				format.html { render action: "edit" }
+				format.json { render json: @advisor.errors, status: :unprocessable_entity }
+
+			end
+		end
+	end
+
+	# DELETE /advisors/1
+	# DELETE /advisors/1.json
+	def destroy
+
+		@advisor = Advisor.find(params[:id])
+		@advisor.destroy
+
+		respond_to do |format|
+
+			format.html { redirect_to advisors_url }
+			format.json { head :no_content }
+
+		end
+	end
 end
